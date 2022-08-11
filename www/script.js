42561ff398cf38e236968bb06c8addacad5f8756
Brandon Daley <bdaleyAWS+personal@gmail.com>
2022-08-10 23:13:44 -0500

    Added some testing to ensure API returns expected values

--- www/script.js
+++ www/script.js
@@ -3,9 +3,6 @@ const countEl = document.getElementById('count');
 
 //When calling the function, I accounted for successful completion and logging of any errors that may come up.
 updateVisitCount()
-	.then(response => {
-		console.log('Updated visitor count successfully.')
-	})
 	.catch(error => {
 		console.log('Function threw an error.')
 		console.error(error)
@@ -16,9 +13,15 @@ updateVisitCount()
 //For response, I used the await keyword to ensure the fetch action is complete before moving on in the code.
 //For data, I used the await keyword to ensure that the response from the fetch is complete before attempting to format it as JSON.
 //I then assigned the value of the JSON formatted visitor count from the API to the 'count' element within my HTML.
-//https://api.countapi.xyz/update/brandon-daley/resume/?amount=1
 async function updateVisitCount() {
 	const response = await fetch('https://oki1grxc66.execute-api.us-east-1.amazonaws.com/count');
 	const data = await response.json();
+	if (Number.isInteger(data) == true) {
+		console.log('Updated visitor count successfully.');
+	}
+	else {
+		console.error('ERROR: The API did not return an expected number value.')
+	}	
 	countEl.innerHTML = data;
 }
+
