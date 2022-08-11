1d49374aec671493570ca1b0204c68ce26cd11dc
Brandon Daley <bdaleyAWS+personal@gmail.com>
2022-08-11 00:33:04 -0500

    Added better notes, built-in testing for the API response and inserting that response into webpage, improved error handling.

--- www/script.js
+++ www/script.js
@@ -3,9 +3,11 @@ const countEl = document.getElementById('count');
 
 //When calling the function, I accounted for successful completion and logging of any errors that may come up.
 updateVisitCount()
+	.then(response => {
+		console.log('updateVisitCount function completed without errors.')
+	})
 	.catch(error => {
-		console.log('Function threw an error.')
-		console.error(error)
+		console.error('ERROR: updateVisitCount function threw an error.')
 	});
 
 //This function fetches current visitor count from API (from DynamoDB database) and assigns it to the response variable.
@@ -15,13 +17,16 @@ updateVisitCount()
 //I then assigned the value of the JSON formatted visitor count from the API to the 'count' element within my HTML.
 async function updateVisitCount() {
 	const response = await fetch('https://oki1grxc66.execute-api.us-east-1.amazonaws.com/count');
-	const data = await response.json();
-	if (Number.isInteger(data) == true) {
-		console.log('Updated visitor count successfully.');
+	const data = await response.json();	
+	countEl.innerHTML = data;
+
+	//This if statement performs some built-in testing. Specifically, it tests to make sure the API returns an expected value.
+	//It also checks to make sure the webpage was actually updated with the returned value.
+	//If both of these tests do not pass, it will log the error in the console.
+	if (Number.isInteger(data) == true && countEl.innerHTML == data) {
+		console.log('Updated visitor count on webpage successfully.');
 	}
 	else {
 		console.error('ERROR: The API did not return an expected number value.')
-	}	
-	countEl.innerHTML = data;
+	}
 }
-
